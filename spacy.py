d3fc017bd84c9d636741d3b3fb55771d7619b963
fullstackdata <chandana@gmail.com>
2021-04-09 14:08:15 -0400

    Update spacy.py

--- spacy.py
+++ spacy.py
@@ -1,5 +1,6 @@
 !pip install -U pip setuptools wheel
 !pip install -U spacy[transformers,lookups]
+!pip install -U spacy[cuda110,transformers,lookups]   - cuda 11 GPU
 !python -m spacy download en_core_web_trf
 
 import spacy
